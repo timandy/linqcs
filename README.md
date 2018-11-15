694ac2f7202b8e8eaa34ef4522f3b765672b41fa
TimAndy <xuchonglei@126.com>
2018-11-15 15:34:52 +0800

    update corefx to `a624719780aa96a0cb33855375a79b611fda1ace`

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 <!--自述文件-->
 
 # Source code of .net linq.
+
+- update corefx to `a624719780aa96a0cb33855375a79b611fda1ace`
---
+++ src/System/Linq/SR.cs
@@ -0,0 +1,12 @@
+﻿namespace System.LinqCore
+{
+    public static class SR
+    {
+        public const String EmptyEnumerable = "Enumeration yielded no results.";
+        public const String MoreThanOneElement = "Sequence contains more than one element.";
+        public const String MoreThanOneMatch = "Sequence contains more than one matching element.";
+        public const String NoElements = "Sequence contains no elements.";
+        public const String NoMatch = "Sequence contains no matching element.";
+        public const String NoSuchElement = "Sequence contains no such element.";
+    }
+}
