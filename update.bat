7c936112bd171fe7bd0840cacfffd65b9cb4adcd
TimAndy <xuchonglei@126.com>
2019-12-05 16:33:59 +0800

    modify update script

--- update.bat
+++ update.bat
@@ -15,16 +15,16 @@ ECHO --------------------------------------------------
 PAUSE
 
 ECHO UPDATE FILES
-XCOPY   /E  ..\corefx\src\System.Linq\ref                                                           .\ref\
-XCOPY   /E  ..\corefx\src\System.Linq\src                                                           .\src\
-XCOPY   /E  ..\corefx\src\System.Linq\tests                                                         .\tests\
-XCOPY   /E  ..\corefx\src\System.Linq\System.Linq.sln                                               .\
-XCOPY   /E  ..\corefx\src\Common\src\System\Collections\Generic\ArrayBuilder.cs                     .\src\System\Collections\Generic\
-XCOPY   /E  ..\corefx\src\Common\src\System\Collections\Generic\EnumerableHelpers.cs                .\src\System\Collections\Generic\
-XCOPY   /E  ..\corefx\src\Common\src\System\Collections\Generic\EnumerableHelpers.Linq.cs           .\src\System\Collections\Generic\
-XCOPY   /E  ..\corefx\src\Common\src\System\Collections\Generic\LargeArrayBuilder.cs                .\src\System\Collections\Generic\
-XCOPY   /E  ..\corefx\src\Common\src\System\Collections\Generic\LargeArrayBuilder.SpeedOpt.cs       .\src\System\Collections\Generic\
-XCOPY   /E  ..\corefx\src\Common\src\System\Collections\Generic\SparseArrayBuilder.cs               .\src\System\Collections\Generic\
+XCOPY   /E  ..\runtime\src\libraries\System.Linq\ref                                                           .\ref\
+XCOPY   /E  ..\runtime\src\libraries\System.Linq\src                                                           .\src\
+XCOPY   /E  ..\runtime\src\libraries\System.Linq\tests                                                         .\tests\
+XCOPY   /E  ..\runtime\src\libraries\System.Linq\System.Linq.sln                                               .\
+XCOPY   /E  ..\runtime\src\libraries\Common\src\System\Collections\Generic\ArrayBuilder.cs                     .\src\System\Collections\Generic\
+XCOPY   /E  ..\runtime\src\libraries\Common\src\System\Collections\Generic\EnumerableHelpers.cs                .\src\System\Collections\Generic\
+XCOPY   /E  ..\runtime\src\libraries\Common\src\System\Collections\Generic\EnumerableHelpers.Linq.cs           .\src\System\Collections\Generic\
+XCOPY   /E  ..\runtime\src\libraries\Common\src\System\Collections\Generic\LargeArrayBuilder.cs                .\src\System\Collections\Generic\
+XCOPY   /E  ..\runtime\src\libraries\Common\src\System\Collections\Generic\LargeArrayBuilder.SpeedOpt.cs       .\src\System\Collections\Generic\
+XCOPY   /E  ..\runtime\src\libraries\Common\src\System\Collections\Generic\SparseArrayBuilder.cs               .\src\System\Collections\Generic\
 ECHO DONE
 ECHO --------------------------------------------------
 
